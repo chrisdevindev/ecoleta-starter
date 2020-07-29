310a5c9a783f313e8973cd0cbe968449a1a61bfd
Christian <alvescastro00cca13@gmail.com>
2020-07-29 17:20:46 -0300

    Adding data to the database

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ server.use(express.static("public"))
 
 // ----------------------------------------------------------------------------------------------------
 
+//HABILITAR O USO DO req.body NA APLICAÇÃO---------------------------------------
+server.use(express.urlencoded({extended:true}))
+// ----------------------------------------------------------------------------------------------------
+
 
 //UTILIZANDO TEMPLATE ENGINE---------------------------------------------------------------------------
 const nunjucks = require("nunjucks")
@@ -35,6 +39,46 @@ server.get("/create-point", (req, res) =>{
 })
 
 
+
+server.post("/savepoint", (req, res) => {
+
+//inserindo dados na tabela
+    const query =`
+    INSERT INTO places(
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+    )VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err){
+        if(err){
+            return console.log(err)
+        }
+
+        console.log("Cadastrado com sucesso!")
+        console.log(this)
+
+        return res.send("DEU BOM")
+    }
+
+    db.run(query, values, afterInsertData)  
+
+})
 
 
 
